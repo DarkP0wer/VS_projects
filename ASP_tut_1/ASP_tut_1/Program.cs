b693a8366fd0c63252163b5461b0dc76fa3bacea
DarkPower <DarkPower@DARKPOWER->
2018-01-19 16:08:14 +0200

    upd

---
+++ ASP_tut_1/ASP_tut_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.IISIntegration;
+
+namespace ASP_tut_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //using ( var host = WebHost.Start("http://localhost:8080", context => context.Response.WriteAsync("Hello World!")))
+            //{
+            //    Console.WriteLine("Application has been started");
+            //    host.WaitForShutdown();
+            //}
+
+
+            //BuildWebHost(args).Run();
+
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .CaptureStartupErrors(true)
+                .UseSetting("detailedErrors", "true")
+                .Build();
+
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args) //IWebHostBuilder
+                .UseStartup<Startup>()
+                .UseIISIntegration()
+                .Build();
+    }
+}
---
+++ ASP_tut_1/ASP_tut_1/TokenMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_tut_1
+{
+    public class TokenMiddleware
+    {
+        RequestDelegate _next;
+        string _pattern;
+
+        public TokenMiddleware(RequestDelegate next, string pattern)
+        {
+            _next = next;
+            _pattern = pattern;
+        }
+
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Query["token"];
+
+            if (token != _pattern)
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Token is invalid");
+            }
+            else
+                await _next(context);
+        }
+    }
+
+    public static class TokenExteinsions
+    {
+        public static IApplicationBuilder UseToken(this IApplicationBuilder builder, string pattern)
+        {
+            return builder.UseMiddleware<TokenMiddleware>(pattern);
+        }
+    }
+}
