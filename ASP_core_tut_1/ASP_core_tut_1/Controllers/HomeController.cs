564c922490667ef0eb03792e8a2c9c3062cf1a0b
Алексей КудряКыапыsfsвыйКу <a.sfsfssf@gmail.com>
2018-01-24 23:32:03 +0200

    upd

---
+++ ASP_core_tut_1/ASP_core_tut_1/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASP_core_tut_1.Models;
+
+namespace ASP_core_tut_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        MobileContext db;
+
+
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+    }
+}
---
+++ ASP_core_tut_1/ASP_core_tut_1/Migrations/20180124205652_Initial.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ASP_core_tut_1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Phones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Company = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Oreders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Address = table.Column<string>(nullable: true),
+                    ContactPhone = table.Column<string>(nullable: true),
+                    PhoneId = table.Column<int>(nullable: false),
+                    User = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Oreders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Oreders_Phones_PhoneId",
+                        column: x => x.PhoneId,
+                        principalTable: "Phones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Oreders_PhoneId",
+                table: "Oreders",
+                column: "PhoneId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Oreders");
+
+            migrationBuilder.DropTable(
+                name: "Phones");
+        }
+    }
+}
